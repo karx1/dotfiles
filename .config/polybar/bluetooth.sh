4b2eb18f99e532b2654515155cd3d420e1c512de
Yash Karandikar <nerdstep710@gmail.com>
2022-07-11 19:24:15 +0530

    Show bluetooth battery percentage

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
+
 if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   echo -e "%{F#66ffffff}\uf293"
 else
   if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then 
+  then
     echo -e "\uf293"
+else
+      BAT=$(bluetoothctl info | grep Percentage | cut -d":" -f 2 | cut -d"(" -f 2 | sed 's/)/%/')
+      echo -e "%{F#81a1c1}\uf293 %{F#ffffff}$BAT"
   fi
-  echo -e "%{F#81a1c1}\uf293"
 fi
 
