4bcd0b116e7e2c21c1af5890828470109cfe5751
Yash Karandikar <nerdstep710@gmail.com>
2024-03-13 23:25:28 -0500

    switch alias and battery pos in bluetooth indicator

--- .config/polybar/bluetooth.sh
+++ .config/polybar/bluetooth.sh
@@ -5,9 +5,9 @@ then
   echo -e "%{F#66ffffff}\uf293"
 elif [ $(echo info | bluetoothctl | grep 'DeviceSet' | wc -c) -eq 0 ]
     then
-      BAT=$(bluetoothctl info | grep Percentage | cut -d":" -f 2 | cut -d"(" -f 2 | sed 's/)/%/' | sed 's/^/ /')
-      ALIAS=$(bluetoothctl info | grep Alias | cut -d":" -f 2 | xargs echo -n)
-      echo -e "%{F#81a1c1}\uf293%{F#ffffff}$BAT ($ALIAS)"
+      BAT=$(bluetoothctl info | grep Percentage | cut -d":" -f 2 | cut -d"(" -f 2 | sed 's/)/%/')
+      ALIAS=$(bluetoothctl info | grep Alias | cut -d":" -f 2)
+      echo -e "%{F#81a1c1}\uf293%{F#ffffff}$ALIAS ($BAT)"
 else
     echo -e "\uf293"
 fi
