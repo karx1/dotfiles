e02e6542921409e37b52c1b6eed3b9494e735e70
Yash Karandikar <nerdstep710@gmail.com>
2024-08-27 23:26:27 -0500

    Use unicode characters to show progress bar

--- .config/polybar/scripts/get_spotify_progress.py
+++ .config/polybar/scripts/get_spotify_progress.py
@@ -5,6 +5,11 @@ import math
 from time import strftime
 from time import gmtime
 
+beginning_bar = "\u2502"
+block = "\u2588"
+dash = "\u2500"
+end_bar = "\u2524"
+
 def ex(cmd):
     # conf: change this player
     player = "spotify"
@@ -22,9 +27,15 @@ position = ex("position")
 duration = float(duration) / 1000000
 position = float(position)
 percentage = (position / duration) * 100
-bars = math.ceil(percentage / 10)
+bars = math.floor(percentage / 10)
+
+if bars == 0:
+    beginning_bar = "\u251c"
+
+if bars == 10:
+    end_bar = beginning_bar
 
 duration = strftime("%-M:%S", gmtime(duration))
 position = strftime("%-M:%S", gmtime(position))
-print("|" + "=" * bars + "-" * (10 - bars) + "|", end=" ")
+print(beginning_bar + block * bars + dash * (10 - bars) + end_bar, end=" ")
 print(position + " / " + duration)
